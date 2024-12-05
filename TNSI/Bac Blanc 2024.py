879831079fd70916934a24bf3e08c67ae1c2e450
TifSec <secheret.tifaine@gmail.com>
2024-12-05 15:24:15 +0100

    correction sur cours de SNT

--- TNSI/Bac Blanc 2024.py
+++ TNSI/Bac Blanc 2024.py
@@ -1,26 +1,26 @@
-# Raph
-def somme_entiers(n):
-    somme = 0
-    for i in range (n+1):
-        somme += i
-    return somme
+# # Raph
+# def somme_entiers(n):
+#     somme = 0
+#     for i in range (n+1):
+#         somme += i
+#     return somme
 
-# print(f"raph -> {somme_entiers(4)}") # Doit retourner 10
+# # print(f"raph -> {somme_entiers(4)}") # Doit retourner 10
 
-# Lucas
-def somme_entiers(n:int) -> int :
-    if n == 0:
-        return 0
-    else:
-        res:int = 0
-        for i in range(n):
-            print(f"i = {i}")
-            print(f"n = {n}")
-            print(f"res = {res}")
-            res += n-i
-        return res
+# # Lucas
+# def somme_entiers(n:int) -> int :
+#     if n == 0:
+#         return 0
+#     else:
+#         res:int = 0
+#         for i in range(n):
+#             print(f"i = {i}")
+#             print(f"n = {n}")
+#             print(f"res = {res}")
+#             res += n-i
+#         return res
         
-# print(f"lucas -> {somme_entiers(4)}") # Doit retourner 10
+# # print(f"lucas -> {somme_entiers(4)}") # Doit retourner 10
 
 def occurence_lettre(une_chaine, une_lettre):
     occurences = 0
