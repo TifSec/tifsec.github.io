a68c7eff8a695805287c4be3a3dee2653702cb93
TifSec <secheret.tifaine@gmail.com>
2025-01-24 10:05:13 +0100

    Ajout d'un plugin python pour docstring auto puis test sur le fichier du bac blanc

--- TNSI/Bac Blanc 2024.py
+++ TNSI/Bac Blanc 2024.py
@@ -23,7 +23,16 @@
 # # print(f"lucas -> {somme_entiers(4)}") # Doit retourner 10
 
 def occurence_lettre(une_chaine, une_lettre):
-    
+    """
+    _summary_
+
+    Args:
+        une_chaine (_type_): _description_
+        une_lettre (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     occurences = 0
     for i in range(len(une_chaine)):
         if une_chaine[i] == une_lettre:
