c8af3d9dd284e7729e3d004b459cf61ac1b74919
TifSec <secheret.tifaine@gmail.com>
2025-05-15 10:05:43 +0200

    modif cours 56 1NSI

--- TNSI/Bac Blanc 2024.py
+++ TNSI/Bac Blanc 2024.py
@@ -22,24 +22,34 @@
         
 # # print(f"lucas -> {somme_entiers(4)}") # Doit retourner 10
 
-def occurence_lettre(une_chaine, une_lettre):
-    """
-    _summary_
+# def occurence_lettre(une_chaine, une_lettre):
+#     """
+#     _summary_
 
-    Args:
-        une_chaine (_type_): _description_
-        une_lettre (_type_): _description_
+#     Args:
+#         une_chaine (_type_): _description_
+#         une_lettre (_type_): _description_
 
-    Returns:
-        _type_: _description_
-    """
-    occurences = 0
-    for i in range(len(une_chaine)):
-        if une_chaine[i] == une_lettre:
-            occurences += 1
-    return occurences
+#     Returns:
+#         _type_: _description_
+#     """
+#     occurences = 0
+#     for i in range(len(une_chaine)):
+#         if une_chaine[i] == une_lettre:
+#             occurences += 1
+#     return occurences
 
-chaine = "plop"
-lettre = "p"
+# chaine = "plop"
+# lettre = "p"
 
-print(f"il y a {occurence_lettre(chaine, lettre)} fois la lettre -> {lettre}")
+# print(f"il y a {occurence_lettre(chaine, lettre)} fois la lettre -> {lettre}")
+
+## Raphael ##
+def est_palindrome(chaine):
+    while len(chaine)>=2:
+        if chaine[0] != chaine[-1]
+            return False
+        chaine = chaine[1:-1]
+    return True
+
+est_palindrome("kayak")
