cd9c1066467390c08397c1db2fceaa24fc20792a
tifsec <secheret.tifaine@gmail.com>
2026-06-17 08:54:00 +0200

    fichier de correction épreuves de BAC 1ere

---
+++ 1NSI/correction_epreuve.py
@@ -0,0 +1,19 @@
+# # Question B1
+# for x in range(4):
+#     for y in range(3):
+#         if x>y:
+#             print(x-y)
+
+# # Question B2
+# t = [2, 8, 9, 2]
+# print([x*x for x in t])
+
+# # Question B3
+# placard = { 'chemise': 3, 'pantalon': 6, 'tee shirt': 7 }
+# placard['chaussette'] = 4
+# placard['chemise'] = 5
+# L = list(placard.values())
+# print(L)
+
+# Question C6
+print([[0] * 3 for i in range(4)])
